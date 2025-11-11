7fae07414ea3bfcb3db8000ad58ca26800cbf60c
anaswara2020 <anaswarapi2003@gmail.com>
2025-11-11 15:17:42 +0530

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const API=axios.create({
-    baseURL:'http://localhost:3000/api'
+    // baseURL:'http://localhost:3000/api'
+    baseURL:'https://ems-server-t5xr.onrender.com/api'
+
 })
 
 
